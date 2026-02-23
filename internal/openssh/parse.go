d4ba4e59009a1d4ead8dda4c68c05ab4ab97484d
OllyKeran <ollykeran@gmail.com>
2026-02-23 20:40:55 +0000

    add private key parsing

---
+++ internal/openssh/parse.go
@@ -0,0 +1,104 @@
+package openssh
+
+import (
+	"encoding/pem"
+	"errors"
+	"math/big"
+
+	ssh "golang.org/x/crypto/ssh"
+)
+
+// https://cvsweb.openbsd.org/src/usr.bin/ssh/PROTOCOL.key?annotate=HEAD
+const openssh_auth_magic = "openssh-key-v1\x00"
+
+// ErrNotOpenSSHKey is returned by ParsePrivateKeyBlob when the data is not an unencrypted OpenSSH private key.
+var ErrNotOpenSSHKey = errors.New("openssh: not an unencrypted OpenSSH private key")
+
+type ParsedKey struct {
+	KeyType    string
+	Comment    string
+	PublicKey  []byte // SSH wire format public key (e.g. for ssh.ParsePublicKey)
+	PrivateKey []byte // PEM-encoded private key
+}
+
+func ParsePrivateKeyBlob(data []byte) (*ParsedKey, error) {
+	block, _ := pem.Decode(data)
+	if block == nil || block.Type != "OPENSSH PRIVATE KEY" {
+		return nil, ErrNotOpenSSHKey
+	}
+	key := block.Bytes
+	if len(key) < len(openssh_auth_magic) || string(key[:len(openssh_auth_magic)]) != openssh_auth_magic {
+		return nil, ErrNotOpenSSHKey
+	}
+
+	var outer struct {
+		CipherName string
+		KdfName    string
+		KdfOpts    string
+		NumKeys    uint32
+		PublicKey  []byte
+		PrivateKey []byte
+		Rest       []byte `ssh:"rest"`
+	}
+	if err := ssh.Unmarshal(key[len(openssh_auth_magic):], &outer); err != nil || outer.NumKeys != 1 {
+		return nil, ErrNotOpenSSHKey
+	}
+	if outer.CipherName != "none" {
+		return nil, ErrNotOpenSSHKey
+	}
+
+	var pk struct {
+		Check1  uint32
+		Check2  uint32
+		Keytype string
+		Rest    []byte `ssh:"rest"`
+	}
+	if err := ssh.Unmarshal(outer.PrivateKey, &pk); err != nil || pk.Check1 != pk.Check2 {
+		return nil, ErrNotOpenSSHKey
+	}
+
+	out := &ParsedKey{KeyType: pk.Keytype, PublicKey: outer.PublicKey, PrivateKey: outer.PrivateKey}
+
+	switch pk.Keytype {
+	case ssh.KeyAlgoED25519:
+		var ed struct {
+			Pub     []byte
+			Priv    []byte
+			Comment string
+			Pad     []byte `ssh:"rest"`
+		}
+		if err := ssh.Unmarshal(pk.Rest, &ed); err != nil {
+			return nil, ErrNotOpenSSHKey
+		}
+		out.Comment = ed.Comment
+	case ssh.KeyAlgoRSA:
+		var rsa struct {
+			N       *big.Int
+			E       *big.Int
+			D       *big.Int
+			Iqmp    *big.Int
+			P       *big.Int
+			Q       *big.Int
+			Comment string
+			Pad     []byte `ssh:"rest"`
+		}
+		if err := ssh.Unmarshal(pk.Rest, &rsa); err != nil {
+			return nil, ErrNotOpenSSHKey
+		}
+		out.Comment = rsa.Comment
+	case ssh.KeyAlgoECDSA256, ssh.KeyAlgoECDSA384, ssh.KeyAlgoECDSA521:
+		var ec struct {
+			Curve   string
+			Pub     []byte
+			D       *big.Int
+			Comment string
+			Pad     []byte `ssh:"rest"`
+		}
+		if err := ssh.Unmarshal(pk.Rest, &ec); err != nil {
+			return nil, ErrNotOpenSSHKey
+		}
+		out.Comment = ec.Comment
+	}
+
+	return out, nil
+}
---
+++ internal/openssh/parse_test.go
@@ -0,0 +1,144 @@
+package openssh
+
+import (
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func marshalOpenSSHWithComment(key interface{}, comment string) []byte {
+	block, err := ssh.MarshalPrivateKey(key, comment)
+	if err != nil {
+		panic(err)
+	}
+	return pem.EncodeToMemory(block)
+}
+
+func TestCommentFromPrivateKeyBlob_RSA(t *testing.T) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "rsa-key-comment"
+	data := marshalOpenSSHWithComment(priv, want)
+	parsed, _ := ParsePrivateKeyBlob(data)
+	got := ""
+	if parsed != nil {
+		got = parsed.Comment
+	}
+	if got != want {
+		t.Errorf("parsed.Comment(RSA) = %q, want %q", got, want)
+	}
+}
+
+func TestCommentFromPrivateKeyBlob_Ed25519(t *testing.T) {
+	_, priv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "ed25519-key-comment"
+	data := marshalOpenSSHWithComment(priv, want)
+	parsed, _ := ParsePrivateKeyBlob(data)
+	got := ""
+	if parsed != nil {
+		got = parsed.Comment
+	}
+	if got != want {
+		t.Errorf("parsed.Comment(Ed25519) = %q, want %q", got, want)
+	}
+}
+
+func TestCommentFromPrivateKeyBlob_ECDSA(t *testing.T) {
+	curves := []struct {
+		name  string
+		curve elliptic.Curve
+	}{
+		{"P-256", elliptic.P256()},
+		{"P-384", elliptic.P384()},
+		{"P-521", elliptic.P521()},
+	}
+	for _, c := range curves {
+		t.Run(c.name, func(t *testing.T) {
+			priv, err := ecdsa.GenerateKey(c.curve, rand.Reader)
+			if err != nil {
+				t.Fatal(err)
+			}
+			want := "ecdsa-" + c.name + "-comment"
+			data := marshalOpenSSHWithComment(priv, want)
+			parsed, _ := ParsePrivateKeyBlob(data)
+			got := ""
+			if parsed != nil {
+				got = parsed.Comment
+			}
+			if got != want {
+				t.Errorf("parsed.Comment(ECDSA %s) = %q, want %q", c.name, got, want)
+			}
+		})
+	}
+}
+
+func TestCommentFromPrivateKeyBlob_NonOpenSSHReturnsEmpty(t *testing.T) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+	der := x509.MarshalPKCS1PrivateKey(priv)
+	block := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: der}
+	data := pem.EncodeToMemory(block)
+	parsed, _ := ParsePrivateKeyBlob(data)
+	got := ""
+	if parsed != nil {
+		got = parsed.Comment
+	}
+	if got != "" {
+		t.Errorf("parsed.Comment(non-OpenSSH) = %q, want \"\"", got)
+	}
+}
+
+func TestParsePrivateKeyBlob_and_CommentFromParsedKey(t *testing.T) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wantComment := "parsed-rsa-comment"
+	data := marshalOpenSSHWithComment(priv, wantComment)
+
+	parsed, err := ParsePrivateKeyBlob(data)
+	if err != nil {
+		t.Fatalf("ParsePrivateKeyBlob: %v", err)
+	}
+	if parsed.KeyType != ssh.KeyAlgoRSA {
+		t.Errorf("KeyType = %q, want %q", parsed.KeyType, ssh.KeyAlgoRSA)
+	}
+	if len(parsed.PrivateKey) == 0 {
+		t.Error("PrivateKey blob should be non-empty")
+	}
+	if parsed.Comment != wantComment {
+		t.Errorf("parsed.Comment = %q, want %q", parsed.Comment, wantComment)
+	}
+}
+
+func TestParsePrivateKeyBlob_NonOpenSSHReturnsError(t *testing.T) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+	der := x509.MarshalPKCS1PrivateKey(priv)
+	block := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: der}
+	data := pem.EncodeToMemory(block)
+
+	parsed, err := ParsePrivateKeyBlob(data)
+	if err != ErrNotOpenSSHKey {
+		t.Errorf("ParsePrivateKeyBlob err = %v, want ErrNotOpenSSHKey", err)
+	}
+	if parsed != nil {
+		t.Errorf("ParsePrivateKeyBlob parsed = %v, want nil", parsed)
+	}
+}
